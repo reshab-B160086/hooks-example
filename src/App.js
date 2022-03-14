6bf6bdd2e7b953ac4add79b2fab31a28e919879c
unknown <b160086@nitsikkim.ac.in>
2022-03-15 01:05:24 +0530

    useState example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App" style={{ padding: 10 }}></div>;
+  const [email, setEmail] = useState("");
+
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+  }
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+  return (
+    <div className="App" style={{ padding: 10 }}>
+      <input value={email} onChange={handleChange} />
+      <p>Email : {email}</p>
+      <input value={name} onChange={handleChangeName} />
+      <p>Name : {name}</p>
+    </div>
+  );
 }
 
 export default App;
